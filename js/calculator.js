7445f1a8fda9d7db2c3d3cc00b7b7fae24efc342
Wayne <wayne.cen@gmail.com>
2021-12-01 01:47:03 -0500

    [WC] Setup HTML, Updated CSS & JS
    Created calculator buttons and display in HTML
    
    Aligned elements with Flexbox in CSS
    
    Created operator functions in JavaScript

---
+++ js/calculator.js
@@ -0,0 +1,24 @@
+// calculator script
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function product(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    return a / b;
+}
+
+function operate(operator, numOne, numTwo) {
+    div
+}
+
+console.log(product(2, 2));
+console.log(division(15, 5));
